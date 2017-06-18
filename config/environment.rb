03933bc3b3cdc1a15153537aac9f5c15be743ea4
John Uzoma <bobonnaaa@gmail.com>
2017-06-18 15:21:04 -0700

    require mailgunner

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,7 @@
 # Load the Rails application.
 require_relative 'application'
 require 'carrierwave/orm/activerecord'
+require 'mailgunner'
 
 # Initialize the Rails application.
 Rails.application.initialize!
